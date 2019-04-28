e404bbad2241535817ef416956b26e564d40fa7e
Eagleglance <pokerslava96@mail.ru>
2019-04-28 21:21:17 +0300

    Spring Boot Lesson

--- src/main/java/com/htp/controller/UserController.java
+++ src/main/java/com/htp/controller/UserController.java
@@ -9,6 +9,7 @@ import com.htp.repository.UserDao;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,9 +44,16 @@ public class UserController {
         return new ResponseEntity<>(userDao.findAll(), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Get user from server by id")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Successful getting user"),
+            @ApiResponse(code = 400, message = "Invalid User ID supplied"),
+            @ApiResponse(code = 401, message = "Lol kek"),
+            @ApiResponse(code = 404, message = "User was not found"),
+            @ApiResponse(code = 500, message = "Server error, something wrong")
+    })
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+    public ResponseEntity<User> getUserById(@ApiParam("User Path Id") @PathVariable Long id) {
         User user = userDao.findById(id);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
